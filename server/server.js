e05d85a9316feceb8b8f894fce77d95a00f14a62
SaTyle <zxcvwifi@gmail.com>
2024-01-21 21:43:38 +0530

    server changs

--- server/server.js
+++ server/server.js
@@ -1,8 +1,70 @@
+// const mongoose = require("mongoose");
+// const express = require("express");
+// const Document = require("./Document");
+
+// const currentUrl = window.location.href;
+// const documentId = currentUrl.split('/').pop();
+
+// require("dotenv").config();
+// mongoose.connect(process.env.MONGO_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+// });
+
+// const app = express(); // Create an Express application
+
+// // Set up a default route for the home page
+// app.get("/", (req, res) => {
+//   res.send("Server is running successfully!");
+// });
+
+// // const server = app.listen(3001, () => {
+// //   console.log("Server is running on port 3001");
+// // });
+
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     // origin: "https://docs-colab-websockets-frontend.vercel.app/",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// const defaultValue = "";
+
+// io.on("connection", (socket) => {
+//   socket.on("get-document", async (documentId) => {
+//     const document = await findOrCreateDocument(documentId);
+//     socket.join(documentId);
+//     socket.emit("load-document", document.data);
+
+//     socket.on("send-changes", (delta) => {
+//       socket.broadcast.to(documentId).emit("receive-changes", delta);
+//     });
+
+//     socket.on("save-document", async (data) => {
+//       await Document.findByIdAndUpdate(documentId, { data });
+//     });
+//   });
+// });
+
+// async function findOrCreateDocument(id) {
+//   if (id == null) return;
+
+//   const document = await Document.findById(id);
+//   if (document) return document;
+//   return await Document.create({ _id: id, data: defaultValue });
+// }
+
+
 const mongoose = require("mongoose");
 const express = require("express");
 const Document = require("./Document");
 
 require("dotenv").config();
+
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -10,21 +72,28 @@ mongoose.connect(process.env.MONGO_URL, {
   useCreateIndex: true,
 });
 
-const app = express(); // Create an Express application
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Set up a default route for the home page
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Server is running successfully!");
 });
 
-const server = app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+app.get("/documents/:documentId", async (req, res) => {
+  const documentId = req.params.documentId;
+  const document = await findOrCreateDocument(documentId);
+  res.json({ data: document.data });
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 const io = require("socket.io")(server, {
   cors: {
-    // origin: "http://localhost:3000",
-    origin: "https://docs-colab-websockets-frontend.vercel.app/",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
@@ -48,12 +117,11 @@ io.on("connection", (socket) => {
 });
 
 async function findOrCreateDocument(id) {
-  if (id == null) return;
+  if (!id) return;
 
   const document = await Document.findById(id);
   if (document) return document;
+  
   return await Document.create({ _id: id, data: defaultValue });
 }
-
-
 
